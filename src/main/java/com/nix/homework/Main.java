1acf8f47b74ee53d4d4a55cf6893e9b5777212cb
Swicherwich <gitgilfoyle@gmail.com>
2020-07-10 00:21:36 +0300

    edited

--- src/main/java/com/nix/homework/Main.java
+++ src/main/java/com/nix/homework/Main.java
@@ -8,8 +8,17 @@ import com.nix.homework.retry.RetryImpl;
 public class Main {
 	private static final Retry<Block> retry = new RetryImpl();
 	
+	/*
+	 * Might work at the first time multiply times
+	 * Keep rerunning main in order to catch more iterations result
+	 */
+	
 	public static void main(String[] args) {
 		Block block = new BlockImpl();
-		retry.retry(block, 10);
+		try {
+			retry.retry(block, 10);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- src/main/java/com/nix/homework/retry/Retry.java
+++ src/main/java/com/nix/homework/retry/Retry.java
@@ -3,5 +3,5 @@ package com.nix.homework.retry;
 import com.nix.homework.block.Block;
 
 public interface Retry <T extends Block> {
-	void retry(T t, int i);
+	void retry(T t, int i) throws Exception;
 }
