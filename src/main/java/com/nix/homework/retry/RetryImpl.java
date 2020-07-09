34912ec1d19a3f8be3789811c35d8ef53a7f618f
Swicherwich <gitgilfoyle@gmail.com>
2020-07-10 00:17:39 +0300

    edited logic

--- src/main/java/com/nix/homework/retry/RetryImpl.java
+++ src/main/java/com/nix/homework/retry/RetryImpl.java
@@ -5,12 +5,12 @@ import com.nix.homework.block.Block;
 public class RetryImpl implements Retry<Block> {
 	
 	@Override
-	public void retry(Block block, int iterations) {
+	public void retry(Block block, int iterations) throws Exception {
 		for(int i = 0; i < iterations; ++i) {
 			try {
 				block.run();
 				if(i == iterations - 1) {
-					System.out.println("Out of iterations: " + iterations);
+					throw new Exception("Out of iterations" + iterations);
 				} else {
 					System.out.printf("[iteration %d] Code has been executed successfully\n", i);
 				}
